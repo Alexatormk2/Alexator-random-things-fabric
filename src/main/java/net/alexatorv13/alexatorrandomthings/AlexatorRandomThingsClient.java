a7547ecb87c670c62d6eebaf507bdfd649d12fc5
BuildTools <unconfigured@null.spigotmc.org>
2023-11-20 13:18:37 +0100

    first commit

---
+++ src/main/java/net/alexatorv13/alexatorrandomthings/AlexatorRandomThingsClient.java
@@ -0,0 +1,10 @@
+package net.alexatorv13.alexatorrandomthings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlexatorRandomThingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/alexatorv13/alexatorrandomthings/AlexatorRandomThingsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.alexatorv13.alexatorrandomthings;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AlexatorRandomThingsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
