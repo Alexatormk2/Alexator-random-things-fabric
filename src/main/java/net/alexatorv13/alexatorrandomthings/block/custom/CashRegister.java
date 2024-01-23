b4670055d528d28c2abc1811160d2a01a5d1bcd7
BuildTools <unconfigured@null.spigotmc.org>
2024-01-23 22:49:49 +0100

    Arreglado los grupos de las recetas

--- src/main/java/net/alexatorv13/alexatorrandomthings/block/custom/CashRegister.java
+++ src/main/java/net/alexatorv13/alexatorrandomthings/block/custom/CashRegister.java
@@ -8,18 +8,48 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.ItemScatterer;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.*;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class CashRegister extends BlockWithEntity {
     private static final VoxelShape SHAPE = Block.createCuboidShape(0.0, 0.0, 0.0, 12.0, 12.0, 16.0);
+
+    public static  final DirectionProperty FACING = Properties.HOPPER_FACING;
+
+
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+
+
 
     public CashRegister(AbstractBlock.Settings settings) {
         super(settings);
